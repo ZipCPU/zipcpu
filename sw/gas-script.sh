dd030cbcb4b845a1185bc0a4f9c86a558c2be9cb
Me <me@example.com>
2016-11-28 21:08:58 -0500

    Simplified the build scripts, removing unnecessary configuration options

--- sw/gas-script.sh
+++ sw/gas-script.sh
@@ -1,5 +1,37 @@
 #!/bin/bash
-
+################################################################################
+##
+## Filename:	gas-script.sh
+##
+## Project:	Zip CPU -- a small, lightweight, RISC CPU soft core
+##
+## Purpose:	To configure binutils to properly build the binutils portion of
+##		the ZipCPU toolchain.
+##
+## Creator:	Dan Gisselquist, Ph.D.
+##		Gisselquist Technology, LLC
+##
+################################################################################
+##
+## Copyright (C) 2016, Gisselquist Technology, LLC
+##
+## This program is free software (firmware): you can redistribute it and/or
+## modify it under the terms of  the GNU General Public License as published
+## by the Free Software Foundation, either version 3 of the License, or (at
+## your option) any later version.
+##
+## This program is distributed in the hope that it will be useful, but WITHOUT
+## ANY WARRANTY; without even the implied warranty of MERCHANTIBILITY or
+## FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+## for more details.
+##
+## License:	GPL, v3, as defined and found on www.gnu.org,
+##		http://www.gnu.org/licenses/gpl.html
+##
+##
+################################################################################
+##
+##
 if [[ ! -d binutils-2.25 ]]
 then
   tar -xjf ./binutils-2.25.tar.bz2
@@ -18,11 +50,10 @@ mkdir -p ${INSTALL_BASE}/tools/lib
 mkdir -p build-gas
 cd build-gas
 AR=ar AS=as	\
-../binutils-2.25/configure --with-gas --prefix=${INSTALL_BASE}/cross-tools \
+../binutils-2.25/configure --with-gas
+	--prefix=${INSTALL_BASE}/cross-tools			\
 	--host=${CLFS_HOST}	--target=${CLFS_TARGET}		\
-	--with-sysroot=${INSTALL_BASE}				\
-	--with-lib-path=${INSTALL_BASE}/tools/lib		\
-	--disable-nls --disable-static --disable-multilib	\
-	--enable-gold=yes --enable-plugins --enable-threads	\
+	--disable-nls		--disable-multilib		\
+	--enable-plugins	--enable-threads		\
 	--disable-werror
 
