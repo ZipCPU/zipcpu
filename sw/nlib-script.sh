c83cf3479756cccda1670493ead0d4aab77f517d
ZipCPU <dgisselq@ieee.org>
2017-01-24 07:12:12 -0500

    Removed extraneous lines from nlib-script

--- sw/nlib-script.sh
+++ sw/nlib-script.sh
@@ -34,20 +34,7 @@ cd build-nlib
 
 CLFS_HOST=$MACHTYPE
 CLFS_TARGET="zip"
-INSTALL_BASE=`pwd`/install
 GCC_BASE=${INSTALL_BASE}/../build-gcc/
-CC_FOR_TARGET=${GCC_BASE}/xgcc
-GCC_FOR_TARGET=${GCC_BASE}/xgcc
-CXX_FOR_TARGET=${GCC_BASE}/xg++
-AR_FOR_TARGET=${INSTALL_BASE}/cross-tools/bin/zip-ar
-AS_FOR_TARGET=${INSTALL_BASE}/cross-tools/bin/zip-as
-LD_FOR_TARGET=${INSTALL_BASE}/cross-tools/bin/zip-ld
-NM_FOR_TARGET=${INSTALL_BASE}/cross-tools/bin/zip-nm
-OBJCOPY_FOR_TARGET=${INSTALL_BASE}/cross-tools/bin/zip-objcopy
-OBJDUMP_FOR_TARGET=${INSTALL_BASE}/cross-tools/bin/zip-objdump
-RANLIB_FOR_TARGET=${INSTALL_BASE}/cross-tools/bin/zip-ranlib
-READELF_FOR_TARGET=${INSTALL_BASE}/cross-tools/bin/zip-readelf
-STRIP_FOR_TARGET=${INSTALL_BASE}/cross-tools/bin/zip-strip
 PATH=$PATH:${INSTALL_BASE}/cross-tools/bin:${GCC_BASE}/gcc
 ../$VERSION-zip/configure --prefix=${INSTALL_BASE}/cross-tools	\
         --target=${CLFS_TARGET}	--host=$MACHTYPE --without-fp
