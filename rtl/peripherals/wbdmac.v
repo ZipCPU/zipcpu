fc272576c249280bae888edf18f91ca25e1eb784
ZipCPU <dgisselq@ieee.org>
2017-01-23 21:59:40 -0500

    Added a gentle user halt/interrupt capability

--- rtl/peripherals/wbdmac.v
+++ rtl/peripherals/wbdmac.v
@@ -83,7 +83,7 @@
 //
 ////////////////////////////////////////////////////////////////////////////////
 //
-// Copyright (C) 2015-2016, Gisselquist Technology, LLC
+// Copyright (C) 2015-2017, Gisselquist Technology, LLC
 //
 // This program is free software (firmware): you can redistribute it and/or
 // modify it under the terms of  the GNU General Public License as published
@@ -168,7 +168,7 @@ module wbdmac(i_clk, i_rst,
 
 	reg	last_read_request, last_read_ack,
 		last_write_request, last_write_ack;
-	reg	trigger, abort;
+	reg	trigger, abort, user_halt;
 
 	initial	dma_state = `DMA_IDLE;
 	initial	o_interrupt = 1'b0;
@@ -221,6 +221,8 @@ module wbdmac(i_clk, i_rst,
 		nread      <= 0;
 		if (abort)
 			dma_state <= `DMA_IDLE;
+		else if (user_halt)
+			dma_state <= `DMA_IDLE;
 		else if (trigger)
 			dma_state <= `DMA_READ_REQ;
 		end
@@ -240,11 +242,14 @@ module wbdmac(i_clk, i_rst,
 						+ {{(AW-1){1'b0}},1'b1};
 		end
 
+		if (user_halt)
+			dma_state <= `DMA_READ_ACK;
 		if (i_mwb_err)
 		begin
 			cfg_len <= 0;
 			dma_state <= `DMA_IDLE;
 		end
+
 		if (abort)
 			dma_state <= `DMA_IDLE;
 		if (i_mwb_ack)
@@ -266,6 +271,8 @@ module wbdmac(i_clk, i_rst,
 			nread <= nread+1;
 			if (last_read_ack) // (nread+1 == nracks)
 				dma_state  <= `DMA_PRE_WRITE;
+			if (user_halt)
+				dma_state <= `DMA_IDLE;
 			if (cfg_incs)
 				cfg_raddr  <= cfg_raddr
 						+ {{(AW-1){1'b0}},1'b1};
@@ -303,6 +310,8 @@ module wbdmac(i_clk, i_rst,
 			nwacks <= nwacks+1;
 			cfg_len <= cfg_len +{(AW){1'b1}}; // -1
 		end
+		if (user_halt)
+			dma_state <= `DMA_WRITE_ACK;
 		if (abort)
 			dma_state <= `DMA_IDLE;
 		end
@@ -332,9 +341,10 @@ module wbdmac(i_clk, i_rst,
 
 	initial	o_interrupt = 1'b0;
 	always @(posedge i_clk)
-		o_interrupt <= (dma_state == `DMA_WRITE_ACK)&&(i_mwb_ack)
-				&&(last_write_ack)
-				&&(cfg_len == {{(AW-1){1'b0}},1'b1});
+		o_interrupt <= ((dma_state == `DMA_WRITE_ACK)&&(i_mwb_ack)
+					&&(last_write_ack)
+					&&(cfg_len == {{(AW-1){1'b0}},1'b1}))
+				||((dma_state != `DMA_IDLE)&&(i_mwb_err));
 
 	initial	cfg_err = 1'b0;
 	always @(posedge i_clk)
@@ -465,6 +475,13 @@ module wbdmac(i_clk, i_rst,
 		abort <= (i_rst)||((i_swb_stb)&&(i_swb_we)
 			&&(i_swb_addr == 2'b00)
 			&&(i_swb_data == 32'hffed0000));
+
+	initial	user_halt = 1'b0;
+	always @(posedge i_clk)
+		user_halt <= ((user_halt)&&(dma_state != `DMA_IDLE))
+			||((i_swb_stb)&&(i_swb_we)&&(dma_state != `DMA_IDLE)
+				&&(i_swb_addr == 2'b00)
+				&&(i_swb_data == 32'hafed0000));
 
 endmodule
 
