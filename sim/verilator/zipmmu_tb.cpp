6f07280e46c79229f5af47aa8cc7599a97c39e7f
ZipCPU <dgisselq@ieee.org>
2017-04-19 13:30:20 -0400

    Adjusted license statement for zipmmu_tb.cpp

--- sim/verilator/zipmmu_tb.cpp
+++ sim/verilator/zipmmu_tb.cpp
@@ -27,6 +27,11 @@
 // FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
 // for more details.
 //
+// You should have received a copy of the GNU General Public License along
+// with this program.  (It's in the $(ROOT)/doc directory.  Run make with no
+// target there if the PDF file isn't present.)  If not, see
+// <http://www.gnu.org/licenses/> for a copy.
+//
 // License:	GPL, v3, as defined and found on www.gnu.org,
 //		http://www.gnu.org/licenses/gpl.html
 //
