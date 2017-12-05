5e2c0af8e1828a25ae68879d810b37472e75f63c
ZipCPU <dgisselq@ieee.org>
2017-12-05 13:46:07 -0500

    Added a (NON)-working pipe memory --- stashing changes w/ commit

--- rtl/core/pipemem.v
+++ rtl/core/pipemem.v
@@ -101,27 +101,27 @@ module	pipemem(i_clk, i_reset, i_pipe_stb, i_lock,
 		default: misaligned <= 1'b0;
 		endcase
 	end else
-			misaligned <= 1'b0;
+		misaligned <= 1'b0;
 
 	always @(posedge i_clk)
 		fifo_oreg[wraddr] <= { i_oreg, i_op[2:1], i_addr[1:0] };
 
 	always @(posedge i_clk)
-		if ((i_reset)||(i_wb_err))
+		if ((i_reset)||((i_wb_err)&&(cyc))||((i_pipe_stb)&&(misaligned)))
 			wraddr <= 0;
 		else if (i_pipe_stb)
 			wraddr <= wraddr + 1'b1;
 	always @(posedge i_clk)
-		if ((i_reset)||(i_wb_err))
+		if ((i_reset)||((i_wb_err)&&(cyc))||((i_pipe_stb)&&(misaligned)))
 			rdaddr <= 0;
 		else if ((i_wb_ack)&&(cyc))
 			rdaddr <= rdaddr + 1'b1;
 	assign	nxt_rdaddr = rdaddr + 1'b1;
 
-	wire	gbl_stb, lcl_stb;
-	assign	lcl_stb = (i_addr[31:24]==8'hff)&&(WITH_LOCAL_BUS)
-				&&(!misaligned);
-	assign	gbl_stb = (!lcl_stb)||(!WITH_LOCAL_BUS)&&(!misaligned);
+	wire	gbl_stb, lcl_stb, lcl_bus;
+	assign	lcl_bus = (i_addr[31:24]==8'hff)&&(WITH_LOCAL_BUS);
+	assign	lcl_stb = (lcl_bus)&&(!misaligned);
+	assign	gbl_stb = ((!lcl_bus)||(!WITH_LOCAL_BUS))&&(!misaligned);
 			//= ((i_addr[31:8]!=24'hc00000)||(i_addr[7:5]!=3'h0));
 
 	initial	cyc = 0;
@@ -139,7 +139,8 @@ module	pipemem(i_clk, i_reset, i_pipe_stb, i_lock,
 			cyc <= 1'b0;
 		end else if (cyc)
 		begin
-			if (((!i_wb_stall)&&(!i_pipe_stb))||(i_wb_err))
+			if (((!i_wb_stall)&&(!i_pipe_stb)&&(!misaligned))
+				||(i_wb_err))
 			begin
 				o_wb_stb_gbl <= 1'b0;
 				o_wb_stb_lcl <= 1'b0;
@@ -149,10 +150,16 @@ module	pipemem(i_clk, i_reset, i_pipe_stb, i_lock,
 				// o_wb_data <= i_data;
 			end
 
-			if (((i_wb_ack)&&(nxt_rdaddr == wraddr))||(i_wb_err))
+			if (((i_wb_ack)&&(nxt_rdaddr == wraddr)
+					&&(!o_wb_stb_gbl)
+					&&(!o_wb_stb_lcl)
+					&&((!i_pipe_stb)||(misaligned)))
+				||(i_wb_err))
 			begin
 				r_wb_cyc_gbl <= 1'b0;
 				r_wb_cyc_lcl <= 1'b0;
+				o_wb_stb_gbl <= 1'b0;
+				o_wb_stb_lcl <= 1'b0;
 				cyc <= 1'b0;
 			end
 		end else if (i_pipe_stb) // New memory operation
@@ -161,7 +168,7 @@ module	pipemem(i_clk, i_reset, i_pipe_stb, i_lock,
 			r_wb_cyc_gbl <= gbl_stb;
 			o_wb_stb_lcl <= lcl_stb;
 			o_wb_stb_gbl <= gbl_stb;
-			cyc <= 1'b1;
+			cyc <= (!misaligned);
 			// o_wb_addr <= i_addr[(AW-1):0];
 			// o_wb_data <= i_data;
 			// o_wb_we <= i_op
@@ -249,7 +256,8 @@ module	pipemem(i_clk, i_reset, i_pipe_stb, i_lock,
 		initial	lock_gbl = 1'b0;
 		initial	lock_lcl = 1'b0;
 		always @(posedge i_clk)
-		if ((i_reset)||((i_wb_err)&&(cyc)))
+		if ((i_reset)||((i_wb_err)&&(cyc))
+			||((i_pipe_stb)&&(misaligned)))
 		begin
 			lock_gbl <= 1'b0;
 			lock_lcl <= 1'b0;
@@ -345,11 +353,11 @@ module	pipemem(i_clk, i_reset, i_pipe_stb, i_lock,
 		end
 
 	always @(posedge i_clk)
-		if ((o_wb_cyc_gbl)&&(i_pipe_stb))
+		if ((r_wb_cyc_gbl)&&(i_pipe_stb))
 			`ASSUME(gbl_stb);
 
 	always @(posedge i_clk)
-		if ((o_wb_cyc_lcl)&&(i_pipe_stb))
+		if ((r_wb_cyc_lcl)&&(i_pipe_stb))
 			`ASSUME(lcl_stb);
 
 	// If stb is false, then either lock is on or there are no more STB's
@@ -372,7 +380,8 @@ module	pipemem(i_clk, i_reset, i_pipe_stb, i_lock,
 			`ASSUME(!i_lock);
 
 	always @(*)
-		if ((WITH_LOCAL_BUS)&&(o_wb_cyc_gbl|o_wb_cyc_lcl)&&(i_pipe_stb))
+		if ((WITH_LOCAL_BUS)&&(o_wb_cyc_gbl|o_wb_cyc_lcl)
+			&&(i_pipe_stb))
 		begin
 			if (o_wb_cyc_lcl)
 				`ASSUME(i_addr[31:24] == 8'hff);
@@ -433,16 +442,21 @@ module	pipemem(i_clk, i_reset, i_pipe_stb, i_lock,
 		assert(cyc == (r_wb_cyc_gbl|r_wb_cyc_lcl));
 
 	always @(posedge i_clk)
+	if ((f_past_valid)&&(!$past(misaligned)))
+	begin
 		if (f_stb)
 			assert(f_pipe_used == f_outstanding + 4'h1);
 		else
 			assert(f_pipe_used == f_outstanding);
+	end
 
 	always @(posedge i_clk)
 		if ((f_past_valid)&&($past(r_wb_cyc_gbl||r_wb_cyc_lcl))
 				&&(!$past(f_stb)))
 			assert(!f_stb);
 
+	always @(*)
+		assert((!lcl_stb)||(!gbl_stb));
 `endif
 endmodule
 //
