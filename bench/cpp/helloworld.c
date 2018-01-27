37603c31a98c1fd6734b1a04752f488d12af2a0c
ZipCPU <dgisselq@ieee.org>
2018-01-27 11:15:01 -0500

    Adjusted ownership of helloworld.c, Gisselquist Technology doesn't own it

--- bench/cpp/helloworld.c
+++ bench/cpp/helloworld.c
@@ -9,26 +9,10 @@
 //	run with no O/S.
 //
 //
-// Creator:	Dan Gisselquist, Ph.D.
-//		Gisselquist Technology, LLC
-//
 ////////////////////////////////////////////////////////////////////////////////
 //
-// Copyright (C) 2015-2016, Gisselquist Technology, LLC
-//
-// This program is free software (firmware): you can redistribute it and/or
-// modify it under the terms of  the GNU General Public License as published
-// by the Free Software Foundation, either version 3 of the License, or (at
-// your option) any later version.
-//
-// This program is distributed in the hope that it will be useful, but WITHOUT
-// ANY WARRANTY; without even the implied warranty of MERCHANTIBILITY or
-// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-// for more details.
-//
-// License:	GPL, v3, as defined and found on www.gnu.org,
-//		http://www.gnu.org/licenses/gpl.html
-//
+// Gisselquist Technology asserts no ownership rights over this particular
+// hello world program.
 //
 ////////////////////////////////////////////////////////////////////////////////
 //
