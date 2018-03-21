4ead2727e1ec8a5d41d0469b32540066d22f45cd
ZipCPU <dgisselq@ieee.org>
2018-03-21 07:57:32 -0400

    Formally proven but untested pfcache, with lots of comments added

--- rtl/core/pfcache.v
+++ rtl/core/pfcache.v
@@ -5,8 +5,30 @@
 // Project:	Zip CPU -- a small, lightweight, RISC CPU soft core
 //
 // Purpose:	Keeping our CPU fed with instructions, at one per clock and
-//		with no stalls.  An unusual feature of this cache is the
-//	requirement that the entire cache may be cleared (if necessary).
+//		with only a minimum number stalls.  The entire cache may also
+//	be cleared (if necessary).
+//
+//	This logic is driven by a couple realities:
+//	1. It takes a clock to read from a block RAM address, and hence a clock
+//		to read from the cache.
+//	2. It takes another clock to check that the tag matches
+//
+//		Our goal will be to avoid this second check if at all possible.
+//		Hence, we'll test on the clock of any given request whether
+//		or not the request matches the last tag value, and on the next
+//		clock whether it new tag value (if it has changed).  Hence,
+//		for anything found within the cache, there will be a one
+//		cycle delay on any branch.
+//
+//
+//	Address Words are separated into three components:
+//	[ Tag bits ] [ Cache line number ] [ Cache position w/in the line ]
+//
+//	On any read from the cache, only the second two components are required.
+//	On any read from memory, the first two components will be fixed across
+//	the bus, and the third component will be adjusted from zero to its
+//	maximum value.
+//
 //
 // Creator:	Dan Gisselquist, Ph.D.
 //		Gisselquist Technology, LLC
@@ -62,7 +84,11 @@ module	pfcache(i_clk, i_reset, i_new_pc, i_clear_cache,
 	localparam	PW=LGCACHELEN-LGLINES; // Size of a cache line
 	localparam	BUSW = 32;	// Number of data lines on the bus
 	localparam	AW=ADDRESS_WIDTH; // Shorthand for ADDRESS_WIDTH
-	input	wire			i_clk, i_reset, i_new_pc;
+	//
+	input	wire			i_clk, i_reset;
+	//
+	// The interface with the rest of the CPU
+	input	wire			i_new_pc;
 	input	wire			i_clear_cache;
 	input	wire			i_stall_n;
 	input	wire	[(AW+1):0]	i_pc;
@@ -70,14 +96,17 @@ module	pfcache(i_clk, i_reset, i_new_pc, i_clear_cache,
 	output	wire	[(AW+1):0]	o_pc;
 	output	wire			o_valid;
 	//
-	output	reg		o_wb_cyc, o_wb_stb;
-	output	wire		o_wb_we;
+	// The wishbone bus interface
+	output	reg			o_wb_cyc, o_wb_stb;
+	output	wire			o_wb_we;
 	output	reg	[(AW-1):0]	o_wb_addr;
 	output	wire	[(BUSW-1):0]	o_wb_data;
 	//
 	input	wire			i_wb_ack, i_wb_stall, i_wb_err;
 	input	wire	[(BUSW-1):0]	i_wb_data;
 	//
+	// o_illegal will be true if this instruction was the result of a bus error
+	// (This is also part of the CPU interface)
 	output	reg			o_illegal;
 	//
 `ifdef	NOT_YET_READY
@@ -106,8 +135,11 @@ module	pfcache(i_clk, i_reset, i_new_pc, i_clear_cache,
 	reg	[(AW-CW-1):0]	tags	[0:((1<<(LGLINES))-1)];
 	reg	[((1<<(LGLINES))-1):0]	vmask;
 
+	reg			r_v_from_pc, r_v_from_last, r_new_request;
+	reg			rvsrc, tagsrc;
+	wire			w_new_request, w_v_from_pc, w_v_from_last;
 	reg	[(AW+1):0]	lastpc;
-	reg	[(CW-1):0]	rdaddr;
+	reg	[(CW-1):0]	wraddr;
 	reg	[(AW-1):CW]	tagvalipc, tagvallst;
 	wire	[(AW-1):CW]	tagval;
 	wire	[(AW-1):PW]	lasttag;
@@ -118,48 +150,107 @@ module	pfcache(i_clk, i_reset, i_new_pc, i_clear_cache,
 	// initial	o_pc = 0;
 	reg	[(BUSW-1):0]	r_pc_cache, r_last_cache;
 	reg	[(AW+1):0]	r_pc, r_lastpc;
-	reg	isrc;
+	reg			isrc;
+	reg	[1:0]		delay;
+	reg			svmask, last_ack, needload, last_addr,
+				bus_abort;
+	reg	[(LGLINES-1):0]	saddr;
 
+
+	/////////////////////////////////////////////////
+	//
+	// Read the instruction from the cache
+	//
+	/////////////////////////////////////////////////
+	//
+	//
+	// We'll read two values from the cache, the first is the value if
+	// i_pc contains the address we want, the second is the value we'd read
+	// if lastpc (i.e. $past(i_pc)) was the address we wanted.
 	initial	r_pc = 0;
 	initial	r_lastpc = 0;
 	always @(posedge i_clk)
 	begin
 		// We don't have the logic to select what to read, we must
 		// read both the value at i_pc and lastpc.  cache[i_pc] is
-		// the value we return if the cache is good, cacne[lastpc] is
-		// the value we return if we've been stalled, weren't valid,
-		// or had to wait a clock or two.  (Remember i_pc can't stop
-		// changing for a clock, so we need to keep track of the last
-		// one from before it stopped.)
+		// the value we return if the last cache request was in the
+		// cache on the last clock, cacne[lastpc] is the value we
+		// return if we've been stalled, weren't valid, or had to wait
+		// a clock or two.
 		//
-		// Here we keep track of which answer we want/need
+		// Part of the issue here is that i_pc is going to increment
+		// on this clock before we know whether or not the cache entry
+		// we've just read is valid.  We can't stop this.  Hence, we
+		// need to read from the lastpc entry.
+		//
+		//
+		// Here we keep track of which answer we want/need.
+		// If we reported a valid value to the CPU on the last clock,
+		// and the CPU wasn't stalled, then we want to use i_pc.
+		// Likewise if the CPU gave us an i_new_pc request, then we'll
+		// want to return the value associated with reading the cache
+		// at i_pc.
 		isrc <= ((r_v)&&(i_stall_n))||(i_new_pc);
 
-		// Here we read both, and select which was write using isrc
-		// on the next clock.
+		// Here we read both cache entries, at i_pc and lastpc.
+		// We'll select from among these cache possibilities on the
+		// next clock
 		r_pc_cache <= cache[i_pc[(CW+1):2]];
 		r_last_cache <= cache[lastpc[(CW+1):2]];
+		//
+		// Let's also register(delay) the r_pc and r_lastpc values
+		// for the next clock, so we can accurately report the address
+		// of the cache value we just looked up.
 		r_pc <= i_pc;
 		r_lastpc <= lastpc;
 	end
+
+	// On our next clock, our result with either be the registered i_pc
+	// value from the last clock (if isrc), otherwise r_lastpc
 	assign	o_pc  = (isrc) ? r_pc : r_lastpc;
+	// The same applies for determining what the next output instruction
+	// will be.  We just read it in the last clock, now we just need to
+	// select between the two possibilities we just read.
 	assign	o_insn= (isrc) ? r_pc_cache : r_last_cache;
 
-	reg	tagsrc;
+
+	/////////////////////////////////////////////////
+	//
+	// Read the tag value associated with this tcache line
+	//
+	/////////////////////////////////////////////////
+	//
+	//
+	// tagsrc is roughly equivalent to isrc above.
+//
+// CAN WE REPLACE TAGSRC WITH ISRC?  THE LOGIC LOOKS IDENTICAL, NOW THAT I
+// REMOVED i_clear_cache FROM THE LOGIC
+//
 	always @(posedge i_clk)
 		// It may be possible to recover a clock once the cache line
 		// has been filled, but our prior attempt to do so has lead
 		// to a race condition, so we keep this logic simple.
-		if (((r_v)&&(i_stall_n))||(i_clear_cache)||(i_new_pc))
+		if (((r_v)&&(i_stall_n))||(i_new_pc))
 			tagsrc <= 1'b1;
 		else
 			tagsrc <= 1'b0;
+
+	//
+	// Read the tag value associated with this i_pc value
 	initial	tagvalipc = 0;
 	always @(posedge i_clk)
 		tagvalipc <= tags[i_pc[(CW+1):PW+2]];
+
+
+	//
+	// Read the tag value associated with the lastpc value, from what
+	// i_pc was when we could not tell if this value was in our cache or
+	// not, or perhaps from when we determined that i was not in the cache.
 	initial	tagvallst = 0;
 	always @(posedge i_clk)
 		tagvallst <= tags[lastpc[(CW+1):PW+2]];
+
+	// Select from between these two values on the next clock
 	assign	tagval = (tagsrc)?tagvalipc : tagvallst;
 
 	// i_pc will only increment when everything else isn't stalled, thus
@@ -167,14 +258,24 @@ module	pfcache(i_clk, i_reset, i_new_pc, i_clear_cache,
 	// us to work in spite of stalls.  For example, if the next address
 	// isn't valid, but the decoder is stalled, get the next address
 	// anyway.
+//
+// WILL THIS LOGIC SIMPLIFY AS WELL?
+//
 	initial	lastpc = 0;
 	always @(posedge i_clk)
-		if (((r_v)&&(i_stall_n))||(i_clear_cache)||(i_new_pc))
+		if (((r_v)&&(i_stall_n))||(i_new_pc))
 			lastpc <= i_pc;
 
 	assign	lasttag = lastpc[(AW+1):PW+2];
 
-	wire	w_v_from_pc, w_v_from_last;
+	/////////////////////////////////////////////////
+	//
+	// Use the tag value to determine if our output instruction will be
+	// valid.
+	//
+	/////////////////////////////////////////////////
+	//
+	//
 	assign	w_v_from_pc = ((i_pc[(AW+1):PW+2] == lasttag)
 				&&(tagvalipc == i_pc[(AW+1):CW+2])
 				&&(vmask[i_pc[(CW+1):PW+2]]));
@@ -183,58 +284,89 @@ module	pfcache(i_clk, i_reset, i_new_pc, i_clear_cache,
 				(tagval == lastpc[(AW+1):CW+2])
 				&&(vmask[lastpc[(CW+1):PW+2]]));
 
-	reg	[1:0]	delay;
-
 	initial	delay = 2'h3;
-	reg	rvsrc;
 	always @(posedge i_clk)
 		if ((i_reset)||(i_clear_cache)||(i_new_pc)||((r_v)&&(i_stall_n)))
 		begin
-			// r_v <= r_v_from_pc;
+			// Source our valid signal from i_pc
 			rvsrc <= 1'b1;
+			// Delay at least two clocks before declaring that
+			// we have an invalid result.  This will give us time
+			// to check the tag value of what's in the cache.
 			delay <= 2'h2;
-		end else if (!r_v) begin // Otherwise, r_v was true and we were
-			// stalled, hence only if ~r_v
+		end else if (!r_v) begin
+			// If we aren't sourcing our valid signal from the
+			// i_pc clock, then we are sourcing it from the
+			// lastpc clock (one clock later).  If r_v still
+			// isn't valid, we may need to make a bus request.
+			// Apply our timer and timeout.
 			rvsrc <= 1'b0;
+
+			// Delay is two once the bus starts, in case the
+			// bus transaction needs to be restarted upon completion
+			// This might happen if, after we start loading the
+			// cache, we discover a branch.  The cache load will
+			// still complete, but the branches address needs to be
+			// the onen we jump to.  This may mean we need to load
+			// the cache twice.
 			if (o_wb_cyc)
 				delay <= 2'h2;
 			else if (delay != 0)
 				delay <= delay + 2'b11; // i.e. delay -= 1;
 		end
-	reg	r_v_from_pc, r_v_from_last;
-	reg	r_new_request;
-	wire	w_new_request;
-	initial	r_new_request = 0;
-	assign	w_new_request = (i_reset)||(i_new_pc)||(i_clear_cache);
-	always @(posedge i_clk)
-		r_new_request <= w_new_request;
-	always @(posedge i_clk)
-		r_v_from_pc   <= (w_v_from_pc)&&(!w_new_request);
-	always @(posedge i_clk)
-		r_v_from_last <= (w_v_from_last)&&(!w_new_request);
 
+	wire	w_invalidate_result;
+	assign	w_invalidate_result = (i_reset)||(i_new_pc)||(i_clear_cache);
+
+	initial	r_new_request = 0;
+	always @(posedge i_clk)
+	begin
+		r_new_request <= w_invalidate_result;
+		r_v_from_pc   <= (w_v_from_pc)&&(!w_invalidate_result);
+		r_v_from_last <= (w_v_from_last)&&(!w_invalidate_result);
+	end
+
+	// Now use rvsrc to determine which of the two valid flags we'll be
+	// using: r_v_from_pc (the i_pc address), or r_v_from_last (the lastpc
+	// address)
 	assign	r_v = ((rvsrc)?(r_v_from_pc):(r_v_from_last))&&(!r_new_request);
 	assign	o_valid = (((rvsrc)?(r_v_from_pc):(r_v_from_last))
 				||((o_illegal)&&(!o_wb_cyc)))
+//
+// DO WE STILL NEED THIS?  If this is captured by the decoder, then we may
+// not need this as combinatorial logic
 			&&(!i_new_pc)&&(!i_reset)&&(!i_clear_cache);
 
-	reg	last_ack;
-	initial	last_ack = 1'b0;
-	always @(posedge i_clk)
-		last_ack <= (o_wb_cyc)&&(
-				(rdaddr[(PW-1):1]=={(PW-1){1'b1}})
-				&&((rdaddr[0])||(i_wb_ack)));
-
-	reg	needload;
+	/////////////////////////////////////////////////
+	//
+	// If the instruction isn't in our cache, then we need to load
+	// a new cache line from memory.
+	//
+	/////////////////////////////////////////////////
+	//
+	//
+//
+// THIS LOOKS LIKE IT REQUIRES AN EXTRA TWO CLOCKS DELAY.  SHOULDn'T WE BE
+// CHECKING FOR DELAY == 1, and push other parts of this check to the next
+// clock?
 	initial	needload = 1'b0;
 	always @(posedge i_clk)
 		needload <= ((!r_v)&&(delay==0)
-			&&((tagvallst != lastpc[(AW+1):CW+2])
-				||(!vmask[lastpc[(CW+1):PW+2]]))
+			// &&((tagvallst != lastpc[(AW+1):CW+2])
+			//	||(!vmask[lastpc[(CW+1):PW+2]]))
+			&&(!w_v_from_last)
+			// Prevent us from reloading an illegal address
+			// (i.e. one that produced a bus error) over and over
+			// and over again
 			&&((!illegal_valid)
 				||(lastpc[(AW+1):PW+2] != illegal_cache)));
 
-	reg	last_addr;
+	//
+	// Working from the rule that you want to keep complex logic out of
+	// a state machine if possible, we calculate a "last_stb" value one
+	// clock ahead of time.  Hence, any time a request is accepted, if
+	// last_stb is also true we'll know we need to drop the strobe line,
+	// having finished requesting a complete cache  line.
 	initial	last_addr = 1'b0;
 	always @(posedge i_clk)
 		if (!o_wb_cyc)
@@ -243,16 +375,31 @@ module	pfcache(i_clk, i_reset, i_new_pc, i_clear_cache,
 				&&((!i_wb_stall)|(o_wb_addr[0])))
 			last_addr <= 1'b1;
 
-	reg	bus_abort;
+	//
+	// "last_ack" is almost identical to last_addr, save that this
+	// will be true on the same clock as the last acknowledgment from the
+	// bus.  The state machine logic will use this to determine when to
+	// get off the bus and end the wishbone bus cycle.
+	initial	last_ack = 1'b0;
+	always @(posedge i_clk)
+		last_ack <= (o_wb_cyc)&&(
+				(wraddr[(PW-1):1]=={(PW-1){1'b1}})
+				&&((wraddr[0])||(i_wb_ack)));
+
+	initial	bus_abort = 1'b0;
 	always @(posedge i_clk)
 		if (!o_wb_cyc)
 			bus_abort <= 1'b0;
 		else if (i_clear_cache)
 			bus_abort <= 1'b1;
 
+	//
+	// Here's the difficult piece of state machine logic--the part that
+	// determines o_wb_cyc and o_wb_stb.  We've already moved most of the
+	// complicated logic off of this statemachine, calculating it one cycle
+	// early.  As a result, this is a fairly easy piece of logic.
 	initial	o_wb_cyc  = 1'b0;
 	initial	o_wb_stb  = 1'b0;
-	initial	rdaddr    = 0;
 	always @(posedge i_clk)
 		if (i_reset)
 		begin
@@ -268,24 +415,37 @@ module	pfcache(i_clk, i_reset, i_new_pc, i_clear_cache,
 			if (((i_wb_ack)&&(last_ack))||(i_wb_err))
 				o_wb_cyc <= 1'b0;
 
-			// else if (rdaddr[(PW-1):1] == {(PW-1){1'b1}})
-			//	tags[lastpc[(CW-1):PW]] <= lastpc[(AW-1):CW];
-
 		end else if (needload)
 		begin
 			o_wb_cyc  <= 1'b1;
 			o_wb_stb  <= 1'b1;
 		end
 
+	// If we are reading from this cache line, then once we get the first
+	// acknowledgement, this cache line has the new tag value
 	always @(posedge i_clk)
-		if ((o_wb_cyc)&&(!last_addr))
+		if ((o_wb_cyc)&&(!last_addr)&&(i_wb_ack))
 			tags[o_wb_addr[(CW-1):PW]] <= o_wb_addr[(AW-1):CW];
+
+
+	// On each acknowledgment, increment the address we use to write into
+	// our cache.  Hence, this is the write address into our cache block
+	// RAM.
+	initial	wraddr    = 0;
 	always @(posedge i_clk)
 		if ((o_wb_cyc)&&(i_wb_ack))
-			rdaddr <= rdaddr + 1;
+			wraddr <= wraddr + 1;
 		else if (!o_wb_cyc)
-			rdaddr <= { lastpc[(CW+1):PW+2], {(PW){1'b0}} };
-			
+			wraddr <= { lastpc[(CW+1):PW+2], {(PW){1'b0}} };
+
+	//
+	// The wishbone request address.  This has meaning anytime o_wb_stb
+	// is active, and needs to be incremented any time an address is
+	// accepted--WITH THE EXCEPTION OF THE LAST ADDRESS.  We need to keep
+	// this steady for that last address, unless the last address returns
+	// a bus error.  In that case, the whole cache line will be marked as
+	// invalid--but we'll need the value of this register to know how
+	// to do that propertly.
 	initial	o_wb_addr = {(AW){1'b0}};
 	always @(posedge i_clk)
 		if ((o_wb_stb)&&(!i_wb_stall)&&(!last_addr))
@@ -293,14 +453,20 @@ module	pfcache(i_clk, i_reset, i_new_pc, i_clear_cache,
 		else if (!o_wb_cyc)
 			o_wb_addr <= { lastpc[(AW+1):PW+2], {(PW){1'b0}} };
 
-	// Can't initialize an array, so leave cache uninitialized
-	// We'll also never get an ack without sys being active, so skip
-	// that check.  Or rather, let's just use o_wb_cyc instead.  This
-	// will work because multiple writes to the same address, ending with
-	// a valid write, aren't a problem.
+	// Since it is impossible to initialize an array, our cache will start
+	// up cache uninitialized.  We'll also never get a valid ack without
+	// cyc being active, although we might get one on the clock after
+	// cyc was active--so we need to test and gate on whether o_wb_cyc
+	// is true.
+	//
+	// wraddr will advance forward on every clock cycle where ack is true,
+	// hence we don't need to check i_wb_ack here.  This will work because
+	// multiple writes to the same address, ending with a valid write,
+	// will always yield the valid write's value only after our bus cycle
+	// is over.
 	always @(posedge i_clk)
-		if (o_wb_cyc) // &&(i_wb_ack)
-			cache[rdaddr] <= i_wb_data;
+	if (o_wb_cyc)
+		cache[wraddr] <= i_wb_data;
 
 	// VMask ... is a section loaded?
 	// Note "svmask".  It's purpose is to delay the vmask setting by one
@@ -308,10 +474,8 @@ module	pfcache(i_clk, i_reset, i_new_pc, i_clear_cache,
 	// before declaring that the cache line is valid.  Without this, the
 	// cache line would get read, and the instruction would read from the
 	// last cache line.
-	reg	svmask;
 	initial	vmask = 0;
 	initial	svmask = 1'b0;
-	reg	[(LGLINES-1):0]	saddr;
 	always @(posedge i_clk)
 		if ((i_reset)||(i_clear_cache))
 		begin
@@ -320,7 +484,7 @@ module	pfcache(i_clk, i_reset, i_new_pc, i_clear_cache,
 		end
 		else begin
 			svmask <= ((o_wb_cyc)&&(i_wb_ack)&&(last_ack));
-			
+
 			if (svmask)
 				vmask[saddr] <= (!bus_abort);
 			if ((!o_wb_cyc)&&(needload))
@@ -335,10 +499,7 @@ module	pfcache(i_clk, i_reset, i_new_pc, i_clear_cache,
 		end
 	always @(posedge i_clk)
 		if ((o_wb_cyc)&&(i_wb_ack))
-			saddr <= rdaddr[(CW-1):PW];
-
-	//
-	//
+			saddr <= wraddr[(CW-1):PW];
 	// MMU code
 	//
 	//
@@ -389,7 +550,13 @@ module	pfcache(i_clk, i_reset, i_new_pc, i_clear_cache,
 		assign	mmu_mskadr = 0;
 	end endgenerate
 `endif
-
+	/////////////////////////////////////////////////
+	//
+	// Handle bus errors here.  If a bus read request
+	// returns an error, then we'll mark the entire
+	// line as having a (valid) illegal value.
+	//
+	/////////////////////////////////////////////////
 	//
 	//
 	//
@@ -580,7 +747,7 @@ module	pfcache(i_clk, i_reset, i_new_pc, i_clear_cache,
 				&&(!$past(i_wb_err)))
 			`ASSUME(f_nacks == (1<<PW));
 		else if (o_wb_cyc)
-			assert(f_nacks[(PW-1):0] == rdaddr);
+			assert(f_nacks[(PW-1):0] == wraddr);
 
 	end
 
