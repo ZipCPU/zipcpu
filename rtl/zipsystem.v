3525a5a3376c263ca8f9b4c6a2266999f72ed4ba
ZipCPU <dgisselq@ieee.org>
2018-05-26 22:18:46 -0400

    Fixed reset address for zipcpu_tb, named generate blocks in zipsys

--- rtl/zipsystem.v
+++ rtl/zipsystem.v
@@ -176,7 +176,7 @@ module	zipsystem(i_clk, i_reset,
 		, o_cpu_debug
 `endif
 		);
-	parameter	RESET_ADDRESS=32'h0100000, ADDRESS_WIDTH=30,
+	parameter	RESET_ADDRESS=32'h1000_0000, ADDRESS_WIDTH=30,
 			LGICACHE=10;
 	parameter [0:0]	START_HALTED=1;
 	parameter	EXTERNAL_INTERRUPTS=1,
@@ -622,13 +622,12 @@ module	zipsystem(i_clk, i_reset,
 	//
 	// Counter Interrupt controller
 	//
-	generate
-	if (EXTERNAL_INTERRUPTS <= 9)
-	begin
+	generate if (EXTERNAL_INTERRUPTS <= 9)
+	begin : ALT_PIC
 		icontrol #(8)	ctri(i_clk, cpu_reset, (ctri_sel),
 					sys_data, ctri_data, alt_int_vector[7:0],
 					ctri_int);
-	end else begin
+	end else begin : ALT_PIC
 		icontrol #(8+(EXTERNAL_INTERRUPTS-9))
 				ctri(i_clk, cpu_reset, (ctri_sel),
 					sys_data, ctri_data,
@@ -638,7 +637,7 @@ module	zipsystem(i_clk, i_reset,
 
 `else	//	INCLUDE_ACCOUNTING_COUNTERS
 
-	generate
+	generate : ALT_PIC
 	if (EXTERNAL_INTERRUPTS <= 9)
 	begin
 		assign	ctri_stall = 1'b0;
@@ -698,15 +697,14 @@ module	zipsystem(i_clk, i_reset,
 	// The programmable interrupt controller peripheral
 	//
 	wire		pic_interrupt;
-	generate
-	if (EXTERNAL_INTERRUPTS < 9)
-	begin
+	generate if (EXTERNAL_INTERRUPTS < 9)
+	begin : MAIN_PIC
 		icontrol #(6+EXTERNAL_INTERRUPTS)	pic(i_clk, cpu_reset,
 					(sys_cyc)&&(sys_stb)&&(sys_we)
 						&&(sel_pic),
 					sys_data, pic_data,
 					main_int_vector[(6+EXTERNAL_INTERRUPTS-1):0], pic_interrupt);
-	end else begin
+	end else begin : MAIN_PIC
 		icontrol #(15)	pic(i_clk, cpu_reset,
 					(sys_cyc)&&(sys_stb)&&(sys_we)
 						&&(sel_pic),
