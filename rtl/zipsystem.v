097c79344952c2caef83f057348d1d18ab2dd445
ZipCPU <dgisselq@ieee.org>
2018-12-18 13:50:02 -0500

    Default to use OPT_DCACHE in ZipSystem

--- rtl/zipsystem.v
+++ rtl/zipsystem.v
@@ -178,7 +178,7 @@ module	zipsystem(i_clk, i_reset,
 		);
 	parameter	RESET_ADDRESS=32'h1000_0000, ADDRESS_WIDTH=30,
 			LGICACHE=10,
-			LGDCACHE=0;	// Set to zero for no data cache
+			LGDCACHE=12;	// Set to zero for no data cache
 	parameter [0:0]	START_HALTED=1;
 	parameter	EXTERNAL_INTERRUPTS=1,
 `ifdef	OPT_MULTIPLY
