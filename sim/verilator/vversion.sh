decacbdc84807b2c2ddca0bdc24c5b22976adb17
ZipCPU <dgisselq@ieee.org>
2021-08-03 10:49:34 -0400

    Update vversion.sh to check for the new Verilator root semantics

--- sim/verilator/vversion.sh
+++ sim/verilator/vversion.sh
@@ -18,7 +18,7 @@
 ## Copyright (C) 2017-2021, Gisselquist Technology, LLC
 ## {{{
 ## This program is free software (firmware): you can redistribute it and/or
-## modify it under the terms of  the GNU General Public License as published
+## modify it under the terms of the GNU General Public License as published
 ## by the Free Software Foundation, either version 3 of the License, or (at
 ## your option) any later version.
 ##
