cbb2f117d35699ed70ff6fd95953284a3333d63e
ZipCPU <dgisselq@ieee.org>
2021-10-01 15:30:24 -0400

    More legal spacing fixes

--- rtl/ex/sfifo.v
+++ rtl/ex/sfifo.v
@@ -16,7 +16,7 @@
 // This program is hereby granted to the public domain.
 // {{{
 // This program is free software (firmware): you can redistribute it and/or
-// modify it under the terms of  the GNU General Public License as published
+// modify it under the terms of the GNU General Public License as published
 // by the Free Software Foundation, either version 3 of the License, or (at
 // your option) any later version.
 //
