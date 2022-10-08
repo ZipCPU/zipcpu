b5f2a546fe9498c69e92e6a8d1690da66cd49f89
ZipCPU <dgisselq@ieee.org>
2022-10-08 10:42:04 -0400

    Divide bug fix: Fixes negative by positive divides

--- rtl/core/div.v
+++ rtl/core/div.v
@@ -1,7 +1,7 @@
 ////////////////////////////////////////////////////////////////////////////////
 //
 // Filename:	div.v
-//
+// {{{
 // Project:	Zip CPU -- a small, lightweight, RISC CPU soft core
 //
 // Purpose:	Provide an Integer divide capability to the Zip CPU.  Provides
@@ -70,11 +70,11 @@
 //		Gisselquist Technology, LLC
 //
 ////////////////////////////////////////////////////////////////////////////////
-//
-// Copyright (C) 2015-2020, Gisselquist Technology, LLC
-//
+// }}}
+// Copyright (C) 2015-2022, Gisselquist Technology, LLC
+// {{{
 // This program is free software (firmware): you can redistribute it and/or
-// modify it under the terms of  the GNU General Public License as published
+// modify it under the terms of the GNU General Public License as published
 // by the Free Software Foundation, either version 3 of the License, or (at
 // your option) any later version.
 //
@@ -87,30 +87,33 @@
 // with this program.  (It's in the $(ROOT)/doc directory.  Run make with no
 // target there if the PDF file isn't present.)  If not, see
 // <http://www.gnu.org/licenses/> for a copy.
-//
+// }}}
 // License:	GPL, v3, as defined and found on www.gnu.org,
+// {{{
 //		http://www.gnu.org/licenses/gpl.html
-//
 //
 ////////////////////////////////////////////////////////////////////////////////
 //
-//
 `default_nettype	none
-//
-// `include "cpudefs.v"
-//
-module	div(i_clk, i_reset, i_wr, i_signed, i_numerator, i_denominator,
-		o_busy, o_valid, o_err, o_quotient, o_flags);
-	parameter		BW=32, LGBW = 5;
-	input	wire		i_clk, i_reset;
-	// Input parameters
-	input	wire		i_wr, i_signed;
-	input	wire [(BW-1):0]	i_numerator, i_denominator;
-	// Output parameters
-	output	reg		o_busy, o_valid, o_err;
-	output	reg [(BW-1):0]	o_quotient;
-	output	wire	[3:0]	o_flags;
+// }}}
+module	div #(
+		parameter		BW=32, LGBW = 5,
+		parameter [0:0]	OPT_LOWPOWER = 1'b0
+	) (
+		// {{{
+		input	wire		i_clk, i_reset,
+		// Input parameters
+		input	wire		i_wr, i_signed,
+		input	wire [(BW-1):0]	i_numerator, i_denominator,
+		// Output parameters
+		output	reg		o_busy, o_valid, o_err,
+		output	reg [(BW-1):0]	o_quotient,
+		output	wire	[3:0]	o_flags
+		// }}}
+	);
 
+	// Local declarations
+	// {{{
 	// r_busy is an internal busy register.  It will clear one clock
 	// before we are valid, so it can't be o_busy ...
 	//
@@ -120,10 +123,14 @@ module	div(i_clk, i_reset, i_wr, i_signed, i_numerator, i_denominator,
 	wire	[(BW):0]	diff; // , xdiff[(BW-1):0];
 	assign	diff = r_dividend[2*BW-2:BW-1] - r_divisor;
 
-	reg		r_sign, pre_sign, r_z, r_c, last_bit;
+	reg			r_sign, pre_sign, r_z, r_c, last_bit;
 	reg	[(LGBW-1):0]	r_bit;
-	reg	zero_divisor;
+	reg			zero_divisor;
+	wire	w_n;
+	// }}}
 
+	// r_busy
+	// {{{
 	// The Divide logic begins with r_busy.  We use r_busy to determine
 	// whether or not the divide is in progress, vs being complete.
 	// Here, we clear r_busy on any reset and set it on i_wr (the request
@@ -137,7 +144,10 @@ module	div(i_clk, i_reset, i_wr, i_signed, i_numerator, i_denominator,
 		r_busy <= 1'b1;
 	else if ((last_bit)||(zero_divisor))
 		r_busy <= 1'b0;
+	// }}}
 
+	// o_busy
+	// {{{
 	// o_busy is very similar to r_busy, save for some key differences.
 	// Primary among them is that o_busy needs to (possibly) be true
 	// for an extra clock after r_busy clears.  This would be that extra
@@ -154,11 +164,17 @@ module	div(i_clk, i_reset, i_wr, i_signed, i_numerator, i_denominator,
 		o_busy <= 1'b0;
 	else if (!r_busy)
 		o_busy <= 1'b0;
+	// }}}
 
+	// zero_divisor
+	// {{{
 	always @(posedge i_clk)
 	if (i_wr)
 		zero_divisor <= (i_denominator == 0);
+	// }}}
 
+	// o_valid
+	// {{{
 	// o_valid is part of the ZipCPU protocol.  It will be set to true
 	// anytime our answer is valid and may be used by the calling module.
 	// Indeed, the ZipCPU will halt (and ignore us) once the i_wr has been
@@ -183,7 +199,10 @@ module	div(i_clk, i_reset, i_wr, i_signed, i_numerator, i_denominator,
 		o_valid <= 1'b1;
 	end else
 		o_valid <= 1'b0;
+	// }}}
 
+	// o_err
+	// {{{
 	// Division by zero error reporting.  Anytime we detect a zero divisor,
 	// we set our output error, and then hold it until we are valid and
 	// everything clears.
@@ -195,9 +214,10 @@ module	div(i_clk, i_reset, i_wr, i_signed, i_numerator, i_denominator,
 		o_err <= 1'b1;
 	else
 		o_err <= 1'b0;
+	// }}}
 
 	// r_bit
-	//
+	// {{{
 	// Keep track of which "bit" of our divide we are on.  This number
 	// ranges from 31 down to zero.  On any write, we set ourselves to
 	// 5'h1f.  Otherwise, while we are busy (but not within the pre-sign
@@ -210,9 +230,10 @@ module	div(i_clk, i_reset, i_wr, i_signed, i_numerator, i_denominator,
 		r_bit <= r_bit + 1'b1;
 	else
 		r_bit <= 0;
+	// }}}
 
 	// last_bit
-	//
+	// {{{
 	// This logic replaces a lot of logic that was inside our giant state
 	// machine with ... something simpler.  In particular, we'll use this
 	// logic to determine if we are processing our last bit.  The only trick
@@ -227,9 +248,10 @@ module	div(i_clk, i_reset, i_wr, i_signed, i_numerator, i_denominator,
 		last_bit <= (r_bit == {(LGBW){1'b1}}-1'b1);
 	else
 		last_bit <= 1'b0;
+	// }}}
 
 	// pre_sign
-	//
+	// {{{
 	// This is part of the state machine.  pre_sign indicates that we need
 	// a extra clock to take the absolute value of our inputs.  It need only
 	// be true for the one clock, and then it must clear itself.
@@ -239,7 +261,10 @@ module	div(i_clk, i_reset, i_wr, i_signed, i_numerator, i_denominator,
 		pre_sign <= 1'b0;
 	else
 		pre_sign <= (i_wr)&&(i_signed)&&((i_numerator[BW-1])||(i_denominator[BW-1]));
+	// }}}
 
+	// r_z
+	// {{{
 	// As a result of our operation, we need to set the flags.  The most
 	// difficult of these is the "Z" flag indicating that the result is
 	// zero.  Here, we'll use the same logic that sets the low-order
@@ -250,8 +275,10 @@ module	div(i_clk, i_reset, i_wr, i_signed, i_numerator, i_denominator,
 		r_z <= 1'b1;
 	else if ((r_busy)&&(!pre_sign)&&(!diff[BW]))
 		r_z <= 1'b0;
+	// }}}
 
 	// r_dividend
+	// {{{
 	// This is initially the numerator.  On a signed divide, it then becomes
 	// the absolute value of the numerator.  We'll subtract from this value
 	// the divisor for every output bit we are looking for--just as with
@@ -263,21 +290,24 @@ module	div(i_clk, i_reset, i_wr, i_signed, i_numerator, i_denominator,
 		// absolute value of the dividend
 		if (r_dividend[BW-1])
 		begin
-			r_dividend[2*BW-2:0] <= {(2*BW-1){1'b1}};
-			r_dividend[BW-1:0] <= -r_dividend[BW-1:0];
+			r_dividend[2*BW-2:0] <= {(2*BW-1){1'b0}};
+			r_dividend[BW:0] <= -{ 1'b1, r_dividend[BW-1:0] };
 		end
 	end else if (r_busy)
 	begin
 		r_dividend <= { r_dividend[2*BW-3:0], 1'b0 };
 		if (!diff[BW])
 			r_dividend[2*BW-2:BW] <= diff[(BW-2):0];
-	end else if (!r_busy)
+	end else if (!r_busy && (!OPT_LOWPOWER || i_wr))
 		// Once we are done, and r_busy is no longer high, we'll
 		// always accept new values into our dividend.  This
 		// guarantees that, when i_wr is set, the new value
 		// is already set as desired.
 		r_dividend <=  { 31'h0, i_numerator };
+	// }}}
 
+	// r_divisor
+	// {{{
 	initial	r_divisor = 0;
 	always @(posedge i_clk)
 	if (i_reset)
@@ -286,10 +316,12 @@ module	div(i_clk, i_reset, i_wr, i_signed, i_numerator, i_denominator,
 	begin
 		if (r_divisor[BW-1])
 			r_divisor <= -r_divisor;
-	end else if (!r_busy)
+	end else if (!r_busy && (!OPT_LOWPOWER || i_wr))
 		r_divisor <= i_denominator;
+	// }}}
 
 	// r_sign
+	// {{{
 	// is a flag for our state machine control(s).  r_sign will be set to
 	// true any time we are doing a signed divide and the result must be
 	// negative.  In that case, we take a final logic stage at the end of
@@ -308,7 +340,10 @@ module	div(i_clk, i_reset, i_wr, i_signed, i_numerator, i_denominator,
 		r_sign <= (r_sign)&&(!zero_divisor);
 	else
 		r_sign <= 1'b0;
+	// }}}
 
+	// o_quotient
+	// {{{
 	initial	o_quotient = 0;
 	always @(posedge i_clk)
 	if (i_reset)
@@ -322,7 +357,10 @@ module	div(i_clk, i_reset, i_wr, i_signed, i_numerator, i_denominator,
 		o_quotient <= -o_quotient;
 	else
 		o_quotient <= 0;
+	// }}}
 
+	// r_c
+	// {{{
 	// Set Carry on an exact divide
 	// Perhaps nothing uses this, but ... well, I suppose we could remove
 	// this logic eventually, just ... not yet.
@@ -332,17 +370,28 @@ module	div(i_clk, i_reset, i_wr, i_signed, i_numerator, i_denominator,
 		r_c <= 1'b0;
 	else
 		r_c <= (r_busy)&&(diff == 0);
+	// }}}
 
+	// w_n
+	// {{{
 	// The last flag: Negative.  This flag is set assuming that the result
 	// of the divide was negative (i.e., the high order bit is set).  This
 	// will also be true of an unsigned divide--if the high order bit is
 	// ever set upon completion.  Indeed, you might argue that there's no
 	// logic involved.
-	wire	w_n;
 	assign w_n = o_quotient[(BW-1)];
+	// }}}
 
 	assign o_flags = { 1'b0, w_n, r_c, r_z };
-
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+//
+// Formal properties
+// {{{
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 `ifdef	FORMAL
 	reg	f_past_valid;
 	initial	f_past_valid = 0;
@@ -385,6 +434,10 @@ module	div(i_clk, i_reset, i_wr, i_signed, i_numerator, i_denominator,
 	if (o_busy)
 		`ASSUME(!i_wr);
 
+	always @(*)
+	if (r_busy)
+		assert(o_busy);
+
 	always @(posedge i_clk)
 	if ((f_past_valid)&&(!$past(i_reset))&&($past(o_busy))&&(!o_busy))
 	begin
@@ -424,8 +477,9 @@ module	div(i_clk, i_reset, i_wr, i_signed, i_numerator, i_denominator,
 
 	always @(*)
 	if ((o_valid)&&(!o_err))
+	begin
 		assert(r_z == ((o_quotient == 0)? 1'b1:1'b0));
-	else if (o_busy)
+	end else if (o_busy)
 		assert(r_z == (((o_quotient&f_bits_set[BW-1:0]) == 0)? 1'b1: 1'b0));
 
 	always @(*)
@@ -465,8 +519,9 @@ module	div(i_clk, i_reset, i_wr, i_signed, i_numerator, i_denominator,
 		&&($past(r_divisor[2*BW-2:BW])==0))
 	begin
 		if ($past(r_divisor) == 0)
+		begin
 			assert(o_err);
-		else if ($past(pre_sign))
+		end else if ($past(pre_sign))
 		begin
 			if ($past(r_dividend[BW-1]))
 				assert(r_dividend == -$past(r_dividend));
@@ -478,8 +533,9 @@ module	div(i_clk, i_reset, i_wr, i_signed, i_numerator, i_denominator,
 			end
 		end else begin
 			if (o_quotient[0])
+			begin
 				assert(r_dividend == $past(diff));
-			else
+			end else
 				assert(r_dividend == $past(r_dividend));
 
 			// r_divisor should shift down on every step
@@ -487,8 +543,9 @@ module	div(i_clk, i_reset, i_wr, i_signed, i_numerator, i_denominator,
 			assert(r_divisor[2*BW-3:0]==$past(r_divisor[2*BW-2:1]));
 		end
 		if ($past(r_dividend) >= $past(r_divisor[BW-1:0]))
+		begin
 			assert(o_quotient[0]);
-		else
+		end else
 			assert(!o_quotient[0]);
 	end
 	*/
@@ -502,6 +559,7 @@ module	div(i_clk, i_reset, i_wr, i_signed, i_numerator, i_denominator,
 		assert((r_divisor == 0) == zero_divisor);
 
 `ifdef	VERIFIC
+	// {{{
 	// Verify unsigned division
 	assert property (@(posedge i_clk)
 		disable iff (i_reset)
@@ -522,9 +580,10 @@ module	div(i_clk, i_reset, i_wr, i_signed, i_numerator, i_denominator,
 			##1 ((r_busy)&&(!o_err)&&(!o_valid))
 			##1 ((o_err)&&(o_valid))));
 
-
+	// }}}
 `endif	// VERIFIC
 `endif
+// }}}
 endmodule
 //
 // How much logic will this divide use, now that it's been updated to
